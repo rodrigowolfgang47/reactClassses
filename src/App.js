54e4fa37483cede97b8016706672941b6cb5e532
Rodrigo <rodrigocosta47@outlook.br>
2022-02-14 16:55:54 -0300

    igualando projeto ao curso

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import ListaDeNotas from './componente/ListaDeNotas';
-import FormularioCadastro from './componente/FormularioCadastro';
-
-function App() {
-  return (
-    <section>
-      <FormularioCadastro />
-      <ListaDeNotas />
-    </section>
-  );
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
